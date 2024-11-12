c69e8bf7a3cffb79a76743c9e9834705d8c3d1bb
H4L1M4T0U <144480724+H4L1M4T0U@users.noreply.github.com>
2024-11-12 17:43:59 -0500

    des
    eese

---
+++ Diaby_Halimatou_Lab_11.cpp
@@ -0,0 +1,65 @@
+// Halimatou Diaby
+//Homework One Part One
+//9/23/24
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <iomanip>
+
+
+
+
+using namespace std;
+int main()
+{
+
+  const int ARRAY_SIZE = 25;
+   	double alpha[ARRAY_SIZE];
+  
+
+int i = 0;
+
+for (i = 0; i <= 26; i++)
+{
+cout << i << " "; 
+i = pow(i,2);
+
+cout<<i<<endl;
+cout<<alpha[i];
+ //Line 1
+//process list[i] //Line 2}
+
+
+}
+
+
+
+    
+
+
+
+
+
+
+cout <<alpha<< endl;
+
+
+
+
+ 
+    
+    
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
